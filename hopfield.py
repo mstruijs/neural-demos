6ad5c5b30b08b42f739283c43ef78699f36d3cda
Martijn Struijs <m.a.c.struijs@student.tue.nl>
2017-03-01 23:40:21 +0100

    Feed the network vectors instead of matrices. Hopfield network works, but Neupy offers too little control over the iterations

--- hopfield.py
+++ hopfield.py
@@ -1,13 +1,15 @@
 import numpy as np
-from neupy import algorithms
+from neupy import algorithms,plots
+import matplotlib.pyplot as plt
 
-dhnet = algorithms.DiscreteHopfieldNetwork(check_limit=False) 
+dhnet = algorithms.DiscreteHopfieldNetwork(mode='async', check_limit=False) 
 
-def ascii_visualise(bin_matrix):
+def ascii_visualise(bin_array, m=10,n=10):
 	'''
-	Basic visualisation for debug purposes
+	Basic visualisation for debug purposes: print binary array as m x n matrix
 	'''
-	for row in bin_matrix.tolist():
+	
+	for row in bin_array.reshape((n,m)).tolist():
 		print(' '.join('.X'[val] for val in row))
 
 def read_data(filename):
@@ -15,17 +17,16 @@ def read_data(filename):
 	Read the training/test data from file and return it in a list of matrices.
 	'''
 	res = [];
-	m = [[]];
+	m = [];
 	rf = open(filename, 'r')
 	for line in rf.readlines():
-		if len(line) == 1:#empty line			
-			res.append(np.matrix(m[:-1]))
-			m = [[]];
+		if len(line) == 1:#empty line	
+			res.append(np.matrix(m))
+			m = [];
 			continue
 		for char in line.strip():
-			m[len(m)-1].append(1 if char=='X' else 0)
-		m.append([])
-	res.append(np.matrix(m[:-1]))
+			m.append(1 if char=='X' else 0)
+	res.append(np.matrix(m))
 	rf.close()
 	return res
 
@@ -37,13 +38,24 @@ def run(input, iterations=None, show=False):
 	if show:
 		ascii_visualise(result)
 	return result
+
+def show_weights():
+	plt.figure(figsize=(14,12))
+	plt.title("Hinton diagram (weights)")
+	plots.hinton(dhnet.weight)
+	plt.show()
+
 	
 if __name__ == "__main__":
 	training_data = read_data("hopfield-numbers-10x10-training.txt")
-	train(training_data[:-1])
+	train(training_data)
+	print(dhnet.weight)
 	test_data = read_data("hopfield-numbers-10x10-test.txt")
-	ascii_visualise(test_data[0])
-	for i in [62,144,232,379]:
+	ascii_visualise(test_data[1])
+	for i in range(1,100,5): 
+	#This doesn't actually stabilise, as the entire iteration is reset for every run.
+	#Unfortunatly, the method provided by Neupy doesn't give enough control over iteration or stopping early.
+	#TODO: Create iterative method for neupy.algorithms.DiscreteHopfieldNetwork
 		print()
-		run(test_data[0],i,show=True)
-		
+		run(test_data[1],iterations=i,show=True)
+	
