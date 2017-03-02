8e7831fb446d514512a13d7dc8e07497789ce03a
Martijn Struijs <m.a.c.struijs@student.tue.nl>
2017-03-02 10:19:24 +0100

    add convergence test + run to convergence. Add documentation

--- hopfield.py
+++ hopfield.py
@@ -35,21 +35,35 @@ def read_data(filename):
 	return res
 
 def train(data):
+	'''
+	Train the network with the supplied data
+	'''
 	dhnet.train(np.concatenate(data, axis = 0))
 	
 def run(input, iterations=None, show=False):
+	'''
+	Run the trained network with the given input, for the specified number of iterations. 
+	Print the the result if `show`
+	'''
 	result = dhnet.predict(input, iterations)
 	if show:
 		ascii_visualise(result)
+		print()
 	return result
 
 def show_weights():
+	'''
+	Plot the weight matrix in a Hinton diagram
+	'''
 	plt.figure(figsize=(14,12))
 	plt.title("Hinton diagram (weights)")
 	plots.hinton(dhnet.weight)
 	plt.show()
 
 def initialise_run(input_data):
+	'''
+	Prepare a controlled iteration on a trained network for the given input
+	'''
 	global iteration,dhnet,output_data,n_features
 	iteration = 0
 	dhnet.discrete_validation(input_data)
@@ -57,18 +71,46 @@ def initialise_run(input_data):
 	_, n_features = input_data.shape
 	output_data = input_data
 	
-def step(step_size=1,show=False):
+def step(step_size=1, show=False):
+	'''
+	Execute `step_size` asynchronous update steps on the initialised network.
+	Print the result if `show`.
+	'''
 	global iteration,dhnet,output_data,n_features
-	for _ in range(step_size):
+	for _ in range(step_size):		
 		iteration+=1
 		position = np.random.randint(0, n_features - 1)
 		raw_new_value = output_data.dot(dhnet.weight[:, position])
 		output_data[:, position] = np.sign(raw_new_value)
 	result = step_function(output_data).astype(int)
 	if show:
+		print("Iteration " + str(iteration) + ":")
 		ascii_visualise(result)
 	return result
-	
+
+def is_stable():
+	'''
+	Return True iff the initialised network has reached a stable output
+	'''
+	global dhnet,output_data,n_features,iteration
+	for position in range(0,n_features-1):
+		raw_new_value = output_data.dot(dhnet.weight[:, position])
+		if np.sign(raw_new_value) != output_data[0][position]:
+			return False
+	return True
+
+def run_to_convergence(input_data, show_list=[]):
+	'''
+	Runs a trained network on `input_data` until it converges to a stable output.
+	Print the intermediate output at all positions in `show_list`.
+	'''
+	initialise_run(input_data)
+	i=0
+	result = None
+	while not(is_stable()):
+		i+=1
+		result=step(show=(i in show_list))
+	return result
 	
 if __name__ == "__main__":
 	training_data = read_data("hopfield-numbers-10x10-training.txt")
@@ -76,10 +118,18 @@ if __name__ == "__main__":
 	#print(dhnet.weight)
 	test_data = read_data("hopfield-numbers-10x10-test.txt")
 	ascii_visualise(test_data[0])
-	initialise_run(test_data[0])
-	for i in range(1,300,5):
-	#This doesn't actually stabilise, as the entire iteration is reset for every run.
-	#Unfortunatly, the method provided by Neupy doesn't give enough control over iteration or stopping early.
-	#TODO: Create iterative method for neupy.algorithms.DiscreteHopfieldNetwork
-		print("Iteration " + str(i) + ":")
-		step(step_size=5,show=True)
+	step_run = False
+	if step_run:
+		initialise_run(test_data[0])
+		for i in range(1,300,5):
+		#This doesn't actually stabilise, as the entire iteration is reset for every run.
+		#Unfortunatly, the method provided by Neupy doesn't give enough control over iteration or stopping early.
+		#TODO: Create iterative method for neupy.algorithms.DiscreteHopfieldNetwork
+			print("Iteration " + str(i) + ":")
+			step(step_size=5,show=True)
+			if is_stable():
+				break
+	else:
+		res = run_to_convergence(test_data[0],[62,144,232,379])
+		print("Iteration " + str(iteration) + ":")
+		ascii_visualise(res)
