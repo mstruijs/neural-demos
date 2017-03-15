aeca6c58684f1ad052ae569b2745513f3df64126
Ziad <ziadbensnaiba@gmail.com>
2017-03-15 20:12:26 +0100

    fix gitignore and untracked files

--- README.md
+++
@@ -1,9 +0,0 @@
-# neural-demos
-Elementary demos of neural network techniques.
-
-Demos available:
-
-- Hopfield network to recognise numbers from binary matrices
-
-# Dependencies
-[NeuPy](https://github.com/itdxer/neupy) >= 0.5.0
--- cmdlinetest.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Hopfield neural network')
-parser.add_argument('-g','--train', help='Training data set path', required=True)
-parser.add_argument('-t','--test', help='Testing data set path', required=True)
-args = vars(parser.parse_args())
-print args['train']
-print args['test']
